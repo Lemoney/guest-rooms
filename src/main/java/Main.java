9c611fb9e3ff43eada79b466c3becc26f5bf175e
Klimek Stoch <klimekstoch10@gmail.com>
2022-03-04 11:51:30 +0100

    Changed language to english

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,52 +4,57 @@ import service.RoomServiceImpl;
 
 import java.util.Scanner;
 
+import static statements.MessageProvider.*;
+
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         RoomServiceImpl roomService = new RoomServiceImpl();
-
         boolean appOn = true;
-
 
         while(appOn) {
             showMainMenu();
 
             switch (scanner.nextInt()) {
+                // shows all rooms
                 case 1 -> System.out.println(roomService.getAllRooms() + "\n");
+                // show more information about room with specific number
                 case 2 -> {
-                    System.out.println("Podaj numer pokoju");
-                    String numer = scanner.next();
-                    System.out.println(roomService.showRoomDetails(numer));
+                    System.out.println(TYPE_ROOM_NUMBER);
+                    String number = scanner.next();
+                    System.out.println(roomService.showRoomDetails(number));
                 }
+                // Add room
                 case 3 -> {
-                    System.out.println("Podaj numer pokoju:");
+                    System.out.println(TYPE_ROOM_NUMBER);
                     Room room = typeRoom();
                     roomService.saveRoom(room);
-                    System.out.println("Dodano pokój: " + roomService.getRoomByRoomNumber(room.getRoomNumber()));
+                    System.out.println("Room added: " + roomService.getRoomByRoomNumber(room.getRoomNumber()));
                 }
+                // Edit room
                 case 4 -> {
-
-                    System.out.println("Podaj numer pokoju który chcesz edytować");
+                    System.out.println(TYPE_ROOM_NUMBER);
                     Room room = typeRoom();
                     System.out.println(room);
                     roomService.editRoom(room);
                 }
+                // Delete room
                 case 5 -> {
-                    System.out.println("Podaj numer pokoju który chcesz usunąć");
-                    String numer = scanner.next();
-                    roomService.deleteRoom(numer);
+                    System.out.println(TYPE_ROOM_NUMBER);
+                    String number = scanner.next();
+                    roomService.deleteRoom(number);
                 }
+                // Showing rooms with specific capacity
                 case 6 -> {
-                    System.out.println("Podaj pojemność pokoi które mają być wyświetlone");
+                    System.out.println(TYPE_ROOM_CAPACITY);
                     int capacity = scanner.nextInt();
                     System.out.println(roomService.showRoomsWithNumberOfCapacity(capacity));
                 }
                 case 0 -> {
                     appOn = false;
-                    System.out.println("Żegnaj kowboju!");
+                    System.out.println(EXIT);
                 }
             }
         }
@@ -57,23 +62,12 @@ public class Main {
 
     private static Room typeRoom() {
         String numer = scanner.next();
-        System.out.println("Podaj ilość pojedynczych łóżek w pokoju:");
+        System.out.println("Type number of single beds:");
         int singlebed = scanner.nextInt();
-        System.out.println("Podaj ilość podwójnych łóżek w pokoju:");
+        System.out.println("Type number of double bed:");
         int doubleBed = scanner.nextInt();
-        System.out.println("Czy pokój posiada balkon? true/false");
+        System.out.println("Does the room contain balcony? true/false");
         boolean balkon = scanner.nextBoolean();
         return new Room(numer, singlebed, doubleBed, balkon);
-    }
-
-    private static void showMainMenu() {
-        System.out.println("----------MENU----------");
-        System.out.println("1. Wyświetl inforamcje o wszystkich pokojach");
-        System.out.println("2. Wyświetl informacje o pokoju");
-        System.out.println("3. Dodaj pokój");
-        System.out.println("4. Edytuj pokój");
-        System.out.println("5. Usuń pokój");
-        System.out.println("6. Pokaż pokoje o określonej pojemności");
-        System.out.println("0. Wyjdź");
     }
 }
--- src/main/java/statements/MessageProvider.java
+++ src/main/java/statements/MessageProvider.java
@@ -1,4 +1,19 @@
 package statements;
 
 public class MessageProvider {
+
+    public static final String TYPE_ROOM_NUMBER = "Type the room number:";
+    public static final String TYPE_ROOM_CAPACITY = "Input capacity to search rooms:";
+    public static final String EXIT = "Good bye!";
+
+    public static void showMainMenu() {
+        System.out.println("----------MENU----------");
+        System.out.println("1. Show all rooms");
+        System.out.println("2. Show room details");
+        System.out.println("3. Add room");
+        System.out.println("4. Edit room");
+        System.out.println("5. Delete room");
+        System.out.println("6. Show rooms with specific capacity");
+        System.out.println("0. Exit");
+    }
 }
