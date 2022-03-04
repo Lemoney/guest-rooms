3ef8385b99ac08830e0c502481c9e5efdb3c66c5
Klimek Stoch <klimekstoch10@gmail.com>
2022-03-04 09:48:19 +0100

    Connected rooms service to database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 import entity.Room;
 import service.RoomServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,8 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Room> rooms = defaultRooms();
-        RoomServiceImpl roomService = new RoomServiceImpl(rooms);
+        RoomServiceImpl roomService = new RoomServiceImpl();
 
         boolean appOn = true;
 
@@ -77,45 +75,5 @@ public class Main {
         System.out.println("5. Usuń pokój");
         System.out.println("6. Pokaż pokoje o określonej pojemności");
         System.out.println("0. Wyjdź");
-    }
-
-
-
-    static List<Room> defaultRooms() {
-        Room room_1 = new Room("1", 1, 1, true);
-        Room room_2 = new Room("2", 1, 2, true);
-        Room room_3 = new Room("3", 2, 1, false);
-        Room room_4 = new Room("4", 0, 2, false);
-        Room room_5 = new Room("5", 0, 0, false);
-        Room room_6 = new Room("6", 2, 1, false);
-        Room room_7 = new Room("7", 2, 1, false);
-        Room room_8 = new Room("8", 1, 2, false);
-        Room room_9 = new Room("9", 1, 2, false);
-        Room room_10 = new Room("10", 1, 2, true);
-        Room room_11 = new Room("11", 1, 1, true);
-        Room room_12 = new Room("12", 2, 1, false);
-        Room room_13 = new Room("13", 0, 1, false);
-        Room room_14 = new Room("14", 0, 1, false);
-        Room room_15 = new Room("15", 1, 1, false);
-        Room room_16 = new Room("16", 1, 1, false);
-
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(room_1);
-        rooms.add(room_2);
-        rooms.add(room_3);
-        rooms.add(room_4);
-        rooms.add(room_5);
-        rooms.add(room_6);
-        rooms.add(room_7);
-        rooms.add(room_8);
-        rooms.add(room_9);
-        rooms.add(room_10);
-        rooms.add(room_11);
-        rooms.add(room_12);
-        rooms.add(room_13);
-        rooms.add(room_14);
-        rooms.add(room_15);
-        rooms.add(room_16);
-        return rooms;
     }
 }
--- src/main/java/service/RoomServiceImpl.java
+++ src/main/java/service/RoomServiceImpl.java
@@ -1,6 +1,8 @@
 package service;
 
+import api.RoomDao;
 import api.RoomService;
+import dao.RoomDaoImpl;
 import entity.Room;
 
 import java.util.ArrayList;
@@ -8,31 +10,26 @@ import java.util.List;
 
 public class RoomServiceImpl implements RoomService {
 
-    List<Room> rooms;
+    RoomDao roomDao = new RoomDaoImpl();
 
-    public RoomServiceImpl(List<Room> rooms) {
-        this.rooms = rooms;
-    }
 
     @Override
     public List<Room> getAllRooms() {
-        return rooms;
+        return roomDao.getAllRooms();
     }
 
     public Room getRoomByRoomNumber(String roomNumber) {
+        List<Room> rooms = getAllRooms();
         for (Room room : rooms) {
-            if (room.getRoomNumber().equals(roomNumber)) {
-                return room;
-            }
+            if (room.getRoomNumber().equals(roomNumber)) return room;
         }
         return null;
     }
 
     public String showRoomDetails(String roomNumber) {
+        List<Room> rooms = getAllRooms();
         for (Room room : rooms) {
-            if (room.getRoomNumber().equals(roomNumber)) {
-                return room.roomDetails();
-            }
+            if (room.getRoomNumber().equals(roomNumber)) return room.roomDetails();
         }
         return null;
     }
@@ -44,11 +41,12 @@ public class RoomServiceImpl implements RoomService {
 
     @Override
     public void saveRoom(Room room) {
-        rooms.add(room);
+        roomDao.saveRoom(room);
     }
 
     @Override
     public void editRoom(String roomNumber, int singleBed, int doubleBed, boolean balcony) {
+        List<Room> rooms = getAllRooms();
         for (Room room : rooms) {
             if (room.getRoomNumber().equals(roomNumber)) {
                 room.setSingleBeds(singleBed);
@@ -59,6 +57,8 @@ public class RoomServiceImpl implements RoomService {
     }
 
     public void editRoom(Room editedRoom) {
+        List<Room> rooms = getAllRooms();
+
         for (Room room : rooms) {
             if(room.getRoomNumber().equals(editedRoom.getRoomNumber())) {
                 room.setSingleBeds(editedRoom.getSingleBeds());
@@ -73,10 +73,11 @@ public class RoomServiceImpl implements RoomService {
 
     @Override
     public void deleteRoom(String roomNumber) {
-        rooms.removeIf(room -> room.getRoomNumber().equals(roomNumber));
+        roomDao.removeRoomByRoomNumber(Integer.parseInt(roomNumber));
     }
 
     public List<Room> showRoomsWithNumberOfCapacity(int capacity) {
+        List<Room> rooms = getAllRooms();
         List<Room> roomList = new ArrayList<>();
         for (Room room : rooms) {
             if (room.getRoomCapacity() == capacity) {
